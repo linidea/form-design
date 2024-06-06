9689b1c8c5016bd2929be825d3cb828ec5f49739
0xduer <0xduer@gmail.com>
2024-06-06 21:46:04 +0800

    feat:修改首页布局

--- src/home/layout/Header.vue
+++ src/home/layout/Header.vue
@@ -1,11 +1,35 @@
 <template>
-  <div>
-123  </div>
+  <div class="nav">
+    <div class="nav-title">LowCode</div>
+    <div class="nav-icon">
+      <MyIcon name="github" size="30" href="https://github.com/linidea" target="_blank"/>
+    </div>
+  </div>
 </template>
 
 <script>
-// import {Github} from "lucide";
+import MyIcon from '@/component/common/icon'
 
 export default {
+  components: {
+    MyIcon
+  }
 }
 </script>
+<style scoped>
+.nav {
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.nav-title {
+  font-size: 30px;
+  font-weight: bold;
+}
+
+.nav-icon {
+  display: flex;
+}
+</style>
