ee155e183bfe428f4e83c55a4b21948b9caa5e07
0xduer <0xduer@gmail.com>
2024-06-10 03:28:51 +0800

    feat:更换 vue 源文件

--- src/main.js
+++ src/main.js
@@ -5,9 +5,18 @@ import router from './router'
 import store from './store'
 
 //
-Vue.config.productionTip = false
 new Vue({
     router,
     store,
     render: h => h(App)
 }).$mount('#app')
+console.info(
+    `
+    __                      __     _            ______            _ 
+   / /_    __  __          / /    (_)   ____   / ____/  __  __   (_)
+  / __ \\  / / / /         / /    / /   / __ \\ / / __   / / / /  / / 
+ / /_/ / / /_/ /         / /___ / /   / / / // /_/ /  / /_/ /  / /  
+/_.___/  \\__, /         /_____//_/   /_/ /_/ \\____/   \\__,_/  /_/   
+        /____/                                                      
+`
+);
