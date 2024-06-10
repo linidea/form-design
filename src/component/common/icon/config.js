d85cd1d8dbc7858f2833459207853fd48de2eeba
0xduer <0xduer@gmail.com>
2024-06-10 23:33:08 +0800

    feat:新增设计器图标

--- src/component/common/icon/config.js
+++ src/component/common/icon/config.js
@@ -3,6 +3,7 @@ import Github from './img/github.svg'
 import Form from './img/form.svg'
 import Table from './img/table.svg'
 import Flow from './img/flow.svg'
+import PencilRuler from './img/pencil-ruler.svg'
 import ChartPie from './img/chart-pie.svg'
 
 export default {
@@ -20,6 +21,9 @@ export default {
     },
     flow: {
         icon: Flow,
+    },
+    pencilRuler: {
+        icon: PencilRuler,
     },
     chart: {
         icon: ChartPie,
